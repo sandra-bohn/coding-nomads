6dd83390077534560f4930d4975833ebf20e1ffd
Sandra Bohn <sandra.bohn@gmail.com>
2021-04-03 08:04:16 -0700

    fix 01_04

--- 01_python_fundamentals/01_04_formula.py
+++ 01_python_fundamentals/01_04_formula.py
@@ -4,6 +4,4 @@ Write the necessary code to print the result of the following formula:
 	(15.7 * 3.6 - 34.9 * 0.9) / (68.9 - 2.1)
 
 '''
-num = 15.7*3.6-34.9*.9
-den = 68.9-2.1
-print(num/den)
+print((15.7 * 3.6 - 34.9 * 0.9) / (68.9 - 2.1))
