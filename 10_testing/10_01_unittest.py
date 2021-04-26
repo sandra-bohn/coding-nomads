5e40ec6ef2d2dbdad224f2868998d5f65dd02775
sandra-bohn <sandra.bohn@gmail.com>
2021-04-25 17:37:07 -0700

    finished testing labs

--- 10_testing/10_01_unittest.py
+++ 10_testing/10_01_unittest.py
@@ -10,3 +10,34 @@ into different files. Note that you will run into an error when attempting to im
 because Python modules can't begin with a number.
 
 '''
+import unittest
+# write a function with input parameters and a return value
+def sequence(first, second):
+    try:
+        return [first, second, second / first, (second / first) / second]
+    except ZeroDivisionError:
+        return 'first number can\'t be 0'
+
+def longsequence(first, second, length):
+    try:
+        sequence = [first, second]
+        while len(sequence) < length:
+            sequence.append(sequence[-1] / sequence[-2])
+        return sequence
+    except ZeroDivisionError:
+        return 'first number can\'t be 0'
+
+class TestResults(unittest.TestCase):
+    # test resulting sequence
+    def test_result(self):
+        self.assertEqual(sequence(3, 2), [3, 2, 0.6666666666666666, 0.33333333333333333])
+    # test exception message
+    def test_except(self):
+        self.assertEqual(sequence(0, 2), 'first number can\'t be 0')
+    # should not pass, exception should be handled
+    def test_error(self):
+        with self.assertRaises(ZeroDivisionError):
+            sequence(0, 2)
+
+if __name__ == '__main__':
+  unittest.main()
--- 10_testing/10_02_tdd.py
+++ 10_testing/10_02_tdd.py
@@ -8,3 +8,34 @@ fail and write tests that will check and prevent failure.
 You do not need to implement the actual functions after writing the tests but you may.
 
 '''
+import unittest
+# replace f with fart
+class TestFart(unittest.TestCase):
+    # test resulting string
+    def test_resultf(self):
+        self.assertEqual(ftofart('forty'), 'fartorty')
+    def test_resultF(self):
+        self.assertEqual(ftofart('Forty'), 'Fartorty')
+    # test handling of non-string input
+    def test_error(self):
+        with self.assertRaises(AttributeError):
+            ftofart(40)
+
+# make all numbers negative
+class TestNegative(unittest.TestCase):
+    # test that negative stays negative
+    def test_resultneg(self):
+        self.assertEqual(makenegative(-10), -10)
+    # test that positive becomes negative
+    def test_resultpos(self):
+        self.assertEqual(makenegative(10), -10)
+    # test that 0 stays 0
+    def test_result0(self):
+        self.assertEqual(makenegative(0), 0)
+    # handle TypeError for non-numeric input
+    def test_error(self):
+        with self.assertRaises(TypeError):
+            makenegative('ten')
+
+if __name__ == '__main__':
+  unittest.main()
