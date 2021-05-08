065d27edfef23acc9825e0bd6d097a9f8954329f
sandra-bohn <sandra.bohn@gmail.com>
2021-05-08 13:24:54 -0700

    args/kwargs labs

--- 16_variable_arguments/16_01_args.py
+++ 16_variable_arguments/16_01_args.py
@@ -2,3 +2,11 @@
 Write a script with a function that demonstrates the use of *args.
 
 '''
+
+def sum(*numbers):
+    ans = 0
+    for i in numbers:
+        ans += i
+    return ans
+
+print(sum(1, 2, 3, 4, 5))
--- 16_variable_arguments/16_02_kwargs.py
+++ 16_variable_arguments/16_02_kwargs.py
@@ -2,3 +2,9 @@
 Write a script with a function that demonstrates the use of **kwargs.
 
 '''
+
+def describe_me(**descriptors):
+    for k, v in descriptors.items():
+        print(f"My {k} is {v}.")
+
+describe_me(name='Sandra', age=39)
