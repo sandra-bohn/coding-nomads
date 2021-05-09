3a8a6b20d8e12b25852a71b70dc1d3ee01fd2e4a
sandra-bohn <sandra.bohn@gmail.com>
2021-05-09 08:48:30 -0700

    decorator labs

--- 17_decorators/17_01_wraps.py
+++ 17_decorators/17_01_wraps.py
@@ -1,3 +1,18 @@
 """
 Write a decorator function that wraps text passed to it in a html <p> tag.
 """
+def add_paragraph(input_func):
+    def wrapper_func(*args):
+        print('<p>')
+        value = input_func(*args)
+        print('<p>')
+        return value
+    return wrapper_func
+
+@add_paragraph
+def fix_case(*args):
+    for i in args:
+        print(i.capitalize())
+    return args
+
+fix_case('hello')
--- 17_decorators/17_02_tags.py
+++ 17_decorators/17_02_tags.py
@@ -3,3 +3,20 @@ Improve the decorator from the previous exercise by allowing it to take
 any specified HTML tag as an input - making it more general.
 
 '''
+def add_html_tag(tag):
+    def decorator_func(input_func):
+        def wrapper_func(*args):
+            print(tag)
+            value = input_func(*args)
+            print(tag)
+            return value
+        return wrapper_func
+    return decorator_func
+
+@add_html_tag('<p>')
+def fix_case(*args):
+    for i in args:
+        print(i.capitalize())
+    return args
+
+fix_case('hello')
